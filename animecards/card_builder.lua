d2c7354e0bfd262270a17e9750f031b753d818db
adxria <adxria@proton.me>
2025-07-06 14:00:15 +0500

    Add sentence trimming and fix line breaks

--- animecards/card_builder.lua
+++ animecards/card_builder.lua
@@ -22,7 +22,7 @@ local function generate_miscinfo(start_time)
 end
 
 local function format_sentence(lines, noteid)
-  local mpv_sentence = string.gsub(string.gsub(lines, '\n+', '<br>'), '\r', '')
+  local mpv_sentence = lines:gsub('^\n+', ''):gsub('\n+$', ''):gsub('\r', ''):gsub('\n+', '<br>')
 
   if opts.HIGHLIGHT_WORD ~= true then
     return mpv_sentence
